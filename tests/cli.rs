7b86b1d9d552a69f4c00bc631d284272147e3912
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-06-29 22:59:06 +0200

    switch to snapshot testing (#27)

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,135 +1,185 @@
-use assert_cmd::cargo::CargoError;
-use assert_cmd::Command;
+use std::process::Command;
+
+use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
+
+fn base_command() -> Command {
+    Command::new(get_cargo_bin("squish"))
+}
 
 // SUCCESSES
-fn command_with_default_usage() -> Result<Command, CargoError> {
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.arg("tests/input.png");
-    cmd.arg("-o=/var/tmp/output.png");
-    Ok(cmd)
-}
 
 #[test]
 fn cli_shows_help() {
     // GIVEN
-    // WHEN
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.arg("--help");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args(["--help"]);
 
+    // WHEN
     // THEN
-    assert!(output.status.success());
-    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-    assert!(stdout.contains("squish lets you resize images via the command line"));
+    assert_cmd_snapshot!(cmd, @r#"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    squish lets you resize images via the command line
+
+    Usage: squish [OPTIONS] <INPUT>
+
+    Arguments:
+      <INPUT>  Local file path, or "cb" for system clipboard
+
+    Options:
+      -w, --width <INTEGER>          Width of resized image [default: 800]
+      -o, --output-file <FILE>       Destination of resized output file
+      -c, --copy-to-clipboard        Whether to copy resized image to clipboard (only supported for PNG images)
+      -b, --blur-strength <INTEGER>  Blur strength [default: 0]
+      -v, --verbose                  Whether to print updates
+      -m, --print-markdown-address   Whether to print address of output file in markdown format
+      -h, --help                     Print help
+
+    ----- stderr -----
+    "#);
 }
 
 #[test]
 fn cli_works_for_local_file() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args(["tests/input.png", "--output-file", "/var/tmp/output.png"]);
 
+    // WHEN
     // THEN
-    assert!(output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    ");
 }
 
 #[test]
 fn cli_works_when_width_provided() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-w=600");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--width",
+        "600",
+    ]);
 
+    // WHEN
     // THEN
-    if !output.status.success() {
-        let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-        println!("stderr: \n{stderr}");
-    }
-    assert!(output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    ");
 }
 
 #[test]
 fn cli_works_when_blurring_requested() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-b=8");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--blur-strength",
+        "8",
+    ]);
 
+    // WHEN
     // THEN
-    if !output.status.success() {
-        let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-        println!("stderr: \n{stderr}");
-    }
-    assert!(output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+
+    ----- stderr -----
+    ");
 }
 
 #[test]
 fn cli_works_when_verbose_output_requested() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-v");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--verbose",
+    ]);
 
+    // WHEN
     // THEN
-    if !output.status.success() {
-        let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-        println!("stderr: \n{stderr}");
-    }
-    assert!(output.status.success());
-    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-    let expected = r#"input image is 1136 px wide and 668 px tall, and is of the format PNG
-resized image is 800 px wide and 470 px tall; size reduced by 50.45%
-written to "/var/tmp/output.png"
-"#;
-    assert_eq!(stdout, expected);
+    assert_cmd_snapshot!(cmd, @r#"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    input image is 1136 px wide and 668 px tall, and is of the format PNG
+    resized image is 800 px wide and 470 px tall; size reduced by 50.45%
+    written to "/var/tmp/output.png"
+
+    ----- stderr -----
+    "#);
 }
 
 #[test]
 fn cli_works_when_markdown_address_requested() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-m");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--print-markdown-address",
+    ]);
 
+    // WHEN
     // THEN
-    if !output.status.success() {
-        let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-        println!("stderr: \n{stderr}");
-    }
-    assert!(output.status.success());
-    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-    assert_eq!(stdout, "![image](/var/tmp/output.png)\n");
+    assert_cmd_snapshot!(cmd, @r"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    ![image](/var/tmp/output.png)
+
+    ----- stderr -----
+    ");
 }
 
 #[test]
 fn cli_works_when_several_flags_are_provided() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-w=400");
-    cmd.arg("-b=4");
-    cmd.arg("-v");
-    cmd.arg("-m");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--width",
+        "400",
+        "--blur-strength",
+        "4",
+        "--verbose",
+        "--print-markdown-address",
+    ]);
 
+    // WHEN
     // THEN
-    if !output.status.success() {
-        let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-        println!("stderr: \n{stderr}");
-    }
-    assert!(output.status.success());
-    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-    let expected = r#"input image is 1136 px wide and 668 px tall, and is of the format PNG
-resized image is 400 px wide and 235 px tall; size reduced by 87.61%
-written to "/var/tmp/output.png"
-![image](/var/tmp/output.png)
-"#;
-    assert_eq!(stdout, expected);
+    assert_cmd_snapshot!(cmd, @r#"
+    success: true
+    exit_code: 0
+    ----- stdout -----
+    input image is 1136 px wide and 668 px tall, and is of the format PNG
+    resized image is 400 px wide and 235 px tall; size reduced by 87.61%
+    written to "/var/tmp/output.png"
+    ![image](/var/tmp/output.png)
+
+    ----- stderr -----
+    "#);
 }
 
 // FAILURES
@@ -137,101 +187,146 @@ written to "/var/tmp/output.png"
 #[test]
 fn cli_fails_if_file_non_existent() {
     // GIVEN
-    // WHEN
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.arg("tests/absent.png");
-    cmd.arg("-o=/var/tmp/output.png");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args(["tests/absent.png", "--output-file", "/var/tmp/output.png"]);
 
+    // WHEN
     // THEN
-    if output.status.success() {
-        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-        println!("stdout: \n{stdout}");
-    }
-    assert!(!output.status.success());
-    let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-    assert!(stderr.contains("Error: couldn't fetch file metadata"));
+    assert_cmd_snapshot!(cmd, @r"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+
+    ----- stderr -----
+    Error: couldn't fetch file metadata
+
+    Caused by:
+        No such file or directory (os error 2)
+    ");
 }
 
 #[test]
 fn cli_fails_if_incorrect_file_provided() {
     // GIVEN
-    // WHEN
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.arg("Cargo.toml");
-    cmd.arg("-o=/var/tmp/output.png");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args(["Cargo.toml", "--output-file", "/var/tmp/output.png"]);
 
+    // WHEN
     // THEN
-    if output.status.success() {
-        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-        println!("stdout: \n{stdout}");
-    }
-    assert!(!output.status.success());
-    let stderr = String::from_utf8(output.stderr).expect("invalid utf-8 stderr");
-    assert!(stderr.contains("Error: couldn't determine the image format"));
+    assert_cmd_snapshot!(cmd, @r"
+    success: false
+    exit_code: 1
+    ----- stdout -----
+
+    ----- stderr -----
+    Error: couldn't determine the image format
+    ");
 }
 
 #[test]
 fn cli_fails_when_incorrect_width_provided() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    let cmd = cmd.arg("-w=blah");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--width",
+        "blah",
+    ]);
 
+    // WHEN
     // THEN
-    if output.status.success() {
-        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-        println!("stdout: \n{stdout}");
-    }
-    assert!(!output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    error: invalid value 'blah' for '--width <INTEGER>': invalid digit found in string
+
+    For more information, try '--help'.
+    ");
 }
 
 #[test]
 fn cli_fails_when_incorrect_blur_strength_provided() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-b=blah");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--blur-strength",
+        "blah",
+    ]);
 
+    // WHEN
     // THEN
-    if output.status.success() {
-        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-        println!("stdout: \n{stdout}");
-    }
-    assert!(!output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    error: invalid value 'blah' for '--blur-strength <INTEGER>': invalid digit found in string
+
+    For more information, try '--help'.
+    ");
 }
 
 #[test]
 fn cli_fails_when_negative_blur_strength_provided() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-b=-1");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--blur-strength",
+        "-1",
+    ]);
 
+    // WHEN
     // THEN
-    if output.status.success() {
-        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-        println!("stdout: \n{stdout}");
-    }
-    assert!(!output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    error: unexpected argument '-1' found
+
+      tip: to pass '-1' as a value, use '-- -1'
+
+    Usage: squish [OPTIONS] <INPUT>
+
+    For more information, try '--help'.
+    ");
 }
 
 #[test]
 fn cli_fails_when_blur_strength_exceeds_upper_threshold() {
     // GIVEN
-    // WHEN
-    let mut cmd = command_with_default_usage().unwrap();
-    cmd.arg("-b=256");
-    let output = cmd.output().expect("running command failed");
+    let mut base_cmd = base_command();
+    let mut cmd = base_cmd.args([
+        "tests/input.png",
+        "--output-file",
+        "/var/tmp/output.png",
+        "--blur-strength",
+        "256",
+    ]);
 
+    // WHEN
     // THEN
-    if output.status.success() {
-        let stdout = String::from_utf8(output.stdout).expect("invalid utf-8 stdout");
-        println!("stdout: \n{stdout}");
-    }
-    assert!(!output.status.success());
+    assert_cmd_snapshot!(cmd, @r"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    error: invalid value '256' for '--blur-strength <INTEGER>': 256 is not in 0..=255
+
+    For more information, try '--help'.
+    ");
 }
